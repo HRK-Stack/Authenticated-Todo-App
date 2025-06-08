60f623abfe9ff0ce160a2a70b3e9201dbf2356b5
HRK-Stack <hrk.stack@gmail.com>
2025-06-08 20:17:04 +0530

    Created DB and User Model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+connectDB();
 
 app.get("/",(req,res) => {
     res.send("hello");
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
